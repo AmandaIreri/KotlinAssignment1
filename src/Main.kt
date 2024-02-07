36fd8fb4e74bc6154bfcc3dd7aeec016943881ac
AmandaIreri <amandairerii@gmail.com>
2024-02-07 22:31:48 +0300

    Kotlin

---
+++ src/Main.kt
@@ -0,0 +1,30 @@
+fun main(){
+        val student1 = "AMANDA IRERI"
+        var age = 20
+        val phoneNumber = +254700891229
+        var weight = 70
+        var kenyan  = true
+
+        println("FULL NAME: AMANDA IRERI")
+        println("AGE: 20")
+        println("PHONE NUMBER: +254700891229")
+        println("WEIGHT: 70")
+        println("NATIONALITY: Kenya")
+
+    val student2 = "GIFT MWANGI"
+    var age2 = 18
+    val phoneNumber2= +254706817891
+    var weight2 = 55.5
+    var kenyan2 = false
+
+    println("FULL NAME: GIFT MWANGI")
+    println("AGE: 20")
+    println("PHONE NUMBER: +25471234567")
+    println("WEIGHT: 55.5")
+    println("NATIONALITY: Rwanda")
+
+
+
+
+    }
+
